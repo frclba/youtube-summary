b3e24335d5ef1343154128167a06ae24fa06a040
Fernando Aguilar <fraguilar@pm.me>
2024-02-09 12:37:08 -0300

    Fix file paths and improve error handling in main_process_youtube_channel()

--- summarizer.py
+++ summarizer.py
@@ -9,6 +9,7 @@ from pydub import AudioSegment
 
 with open ("current_video_file.dat", "r") as current_video_file:
     CURRENT_VIDEO_PROCCESSING = int(current_video_file.read())
+    current_video_file.close()
 
 def pretty_print(text, width):
     lines = text.splitlines()
@@ -19,7 +20,7 @@ def pretty_print(text, width):
 def download_audio(audio_filename, url):
     # Download from YouTube
     ydl_opts = {
-        'outtmpl': audio_filename,
+        'outtmpl': "temp/"+audio_filename,
         'format': 'worstaudio',
         'postprocessors': [{
             'key': 'FFmpegExtractAudio',
@@ -43,7 +44,7 @@ def summarize_youtube(title, transcription):
     return summary
 
 def chunk_big_file(audio_filename):
-    audio = AudioSegment.from_mp3(audio_filename+".mp3")
+    audio = AudioSegment.from_mp3("temp/"+audio_filename+".mp3")
     audio_length = len(audio) / 1000
     chunk_size = 1 * 60
     num_chunks = int(audio_length // chunk_size)
@@ -53,61 +54,63 @@ def chunk_big_file(audio_filename):
         start_time = i * chunk_size
         end_time = (i + 1) * chunk_size
         chunk = audio[start_time * 1000:end_time * 1000]
-        chunk.export(f"chunk_{i}.mp3", format="mp3")
+        chunk.export(f"temp/chunk_{i}.mp3", format="mp3")
 
     # Handle remaining time
     if remaining_time > 0:
         start_time = num_chunks * chunk_size
         end_time = start_time + remaining_time
         chunk = audio[start_time * 1000:end_time * 1000]
-        chunk.export(f"chunk_{num_chunks}.mp3", format="mp3")
+        chunk.export(f"temp/chunk_{num_chunks}.mp3", format="mp3")
         num_chunks += 1
 
     return num_chunks
 
 def cleanup(audio_filename, num_chunks):
     try:
-        os.remove(audio_filename+".mp3")
-        os.remove(audio_filename+"_mono.mp3")
+        os.remove("temp/"+audio_filename+".mp3")
+        os.remove("temp/"+audio_filename+"_mono.mp3")
     except:
         pass
     for i in range(num_chunks):
         try:
-            os.remove(f"chunk_{i}.mp3")
+            os.remove(f"temp/chunk_{i}.mp3")
         except:
             continue
 
 
 def main_process_youtube_channel():
     youtube_videos = pd.read_csv("youtubeChannel.csv")
-    for i in range(CURRENT_VIDEO_PROCCESSING, len(youtube_videos)):
-        try:
-            print("processing video ", i)
-            url = youtube_videos["Video url"][i]
-            video_title = youtube_videos["Video Title"][i].replace(" ", "_").replace(":", "_").replace("|", "_").replace("?", "_").replace("!", "_").replace(";", "_").replace(",", "_").replace(".", "_").replace("/", "_")
-            if "temp" not in os.getcwd():
-                os.chdir("./temp/")
-            print("Donwloading audio from YouTube...")
-            download_audio(video_title, url)
-            print("chunking audio...")
-            num_chunks = chunk_big_file(video_title)
-            print("Initializing whisper...")
-            model, enc = init_whisper("large-v2", batch_size=1)
-            print("Transcribing audio... with", num_chunks, " chunks")
-            for i in range(num_chunks):
-                chunk_filename = f"chunk_{i}.mp3"
-                try:
-                    output = whisper_transcription(model, enc, chunk_filename)
-                    with open(video_title+"_transcription.txt", "a") as text_file:
-                        text_file.write(output)
-                except:
-                    continue
-            print("Cleaning up...: ")
-            cleanup(video_title, num_chunks)
-            with open ("current_video_file.dat", "w") as current_video_file:
-                current_video_file.write(str(i+1))
-        except:
-            main_process_youtube_channel()
+    try:
+        for video_number in range(CURRENT_VIDEO_PROCCESSING, len(youtube_videos)):
+                print("processing video ", video_number)
+                url = youtube_videos["Video url"][video_number]
+                video_title = youtube_videos["Video Title"][video_number].replace(" ", "_").replace(":", "_").replace("|", "_").replace("?", "_").replace("!", "_").replace(";", "_").replace(",", "_").replace(".", "_").replace("/", "_")
+                print("Donwloading audio from YouTube...")
+                download_audio(video_title, url)
+                print("chunking audio...")
+                num_chunks = chunk_big_file(video_title)
+                print("Initializing whisper...")
+                model, enc = init_whisper("large-v2", batch_size=1)
+                print("Transcribing audio... with", num_chunks, " chunks")
+                for chunk in range(num_chunks):
+                    chunk_filename = f"temp/chunk_{chunk}.mp3"
+                    try:
+                        output = whisper_transcription(model, enc, chunk_filename)
+                        with open("txt/"+video_title+"_transcription.txt", "a") as text_file:
+                            text_file.write(output)
+                    except:
+                        continue
+                print("Cleaning up...: ")
+                cleanup(video_title, num_chunks)
+                with open ("current_video_file.dat", "w") as current_video_file:
+                    current_video_file.write(str(video_number+1))
+    except KeyboardInterrupt:
+        print("\nProgram terminated by the user. Exiting...")
+        exit(0)
+    finally:
+        print("Trying again...")
+        main_process_youtube_channel()
 
 def main():
     parser = argparse.ArgumentParser(description='Summarize YouTube videos.')
@@ -127,19 +130,19 @@ def main():
     model, enc = init_whisper("large-v2", batch_size=1)
     print("Transcribing audio... with", num_chunks, " chunks")
     for i in range(num_chunks):
-        chunk_filename = f"chunk_{i}.mp3"
+        chunk_filename = f"temp/chunk_{i}.mp3"
         try:
             output = whisper_transcription(model, enc, chunk_filename)
-            with open(video_title+"_transcription.txt", "a") as text_file:
+            with open("temp/"+video_title+"_transcription.txt", "a") as text_file:
                 text_file.write(output)
         except:
             continue
 
     print("Summarizing with GPT-4...")
-    with open (video_title+"_transcription.txt", "r") as file:
+    with open("temp/"+video_title+"_transcription.txt", "r") as file:
         data = file.read()
     summary = summarize_youtube(video_title, data)
-    with open(video_title+"_summary.txt", "w") as text_file:
+    with open("temp/"+video_title+"_summary.txt", "w") as text_file:
         text_file.write(summary)
 
     print("Cleaning up...: ")
